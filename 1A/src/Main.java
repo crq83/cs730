e0ee0a033dae7eef69d9aff4c924f065d3f4fe60
crq83 <crq83@wildcats.unh.edu>
2016-09-01 15:00:17 -0400

    reads file from input arguments, easier to debug with intellij

--- 1A/src/Main.java
+++ 1A/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Vector;
@@ -16,12 +17,12 @@ public class Main {
                 useUniformCost = false;
                 break;
         }
-        Vector<String> textFromFile = openFile();
+        Vector<String> textFromFile = openFile(args[1]);
         World world = new World(textFromFile);
     }
 
-    private static Vector<String> openFile() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Vector<String> openFile(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         Vector<String> data = new Vector<>();
 
         String line = reader.readLine();
